7144486a2972ab4951fcd0c4f2596b00d7e84457
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-05 15:40:23 -0500

    Adding styles to pressed/clicked button

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 window.addEventListener('keydown', function(e) { 
-   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   
-   // stop the function from running all together
-   if (!audio) return;
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  // stop the function from running all together
+  if (!audio) return;
 
-   audio.play();
+  // rewind to the start
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
 });
