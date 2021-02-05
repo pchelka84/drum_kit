e4c66ff0e173c417ed1d2d6f5f944f8a50b0153d
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-02-05 16:10:45 -0500

    Extracting playSound function

--- scripts.js
+++ scripts.js
@@ -1,22 +1,24 @@
 const keys = document.querySelectorAll('.key');
 
-window.addEventListener('keydown', function(e) { 
+
+function playSound(e) { 
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   // stop the function from running all together
   if (!audio) return;
-
+  
   // rewind to the start
   audio.currentTime = 0;
   audio.play();
   key.classList.add('playing');
-});
+}
 
 function removeTransition(e) {
   // skipp it if isn't transform
   if (e.propertyName !== 'transform') return;
-
- this.classList.remove('playing');
+  
+  this.classList.remove('playing');
 }
 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
